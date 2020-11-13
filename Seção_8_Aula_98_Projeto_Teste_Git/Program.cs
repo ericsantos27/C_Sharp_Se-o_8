70f0cc6089576179c1def6b46100b50da5c0e29e
Eric Santos <esantos_samu@hotmail.com>
2020-11-13 16:54:02 -0300

    Projeto iniciado

---
+++ Seção_8_Aula_98_Projeto_Teste_Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Seção_8_Aula_98_Projeto_Teste_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aula introdutória Git");
+        }
+    }
+}
