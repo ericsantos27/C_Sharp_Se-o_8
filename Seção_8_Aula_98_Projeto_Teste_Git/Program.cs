55146f344fbc42569fbf892e8670d4287cf7eb51
Eric Santos <esantos_samu@hotmail.com>
2020-11-13 17:02:07 -0300

    Inclusão de código para teste

--- Seção_8_Aula_98_Projeto_Teste_Git/Program.cs
+++ Seção_8_Aula_98_Projeto_Teste_Git/Program.cs
@@ -7,6 +7,8 @@ namespace Seção_8_Aula_98_Projeto_Teste_Git
         static void Main(string[] args)
         {
             Console.WriteLine("Aula introdutória Git");
+            Console.WriteLine("Comentário para rastreamento do git");
+            Console.ReadLine();
         }
     }
 }
